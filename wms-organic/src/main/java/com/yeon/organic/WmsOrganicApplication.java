07e3b3ad07c9b614080358beac02c4aa2fee65e9
DISNOTACAT <yeonjh915@gmail.com>
2024-09-19 01:13:35 +0900

    init project

---
+++ wms-organic/src/main/java/com/yeon/organic/WmsOrganicApplication.java
@@ -0,0 +1,13 @@
+package com.yeon.organic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WmsOrganicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WmsOrganicApplication.class, args);
+    }
+
+}
---
+++ wms-organic/src/test/java/com/yeon/organic/WmsOrganicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yeon.organic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WmsOrganicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
